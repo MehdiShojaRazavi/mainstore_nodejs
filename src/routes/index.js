b6f85eb6e68d93de1628ff26c003e97002c08309
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-02 11:31:44 +0300

    add developer route for internal developer tools

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const { StatusCodes:HttpStatus} = require("http-status-codes")
 const userRouter = require('./user/index');
 const authRouter = require('./auth/index');
+const devloperRouter = require('./developer/index');
 const homeRouter = require('./home/index');
 router.use('/user', userRouter);
 router.use('/auth', authRouter);
+router.use('/developer', devloperRouter);
 router.use('/', homeRouter);
 
 module.exports = router;
