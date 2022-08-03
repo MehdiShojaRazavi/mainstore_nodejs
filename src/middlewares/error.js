df7c78f57bada188a039158c322a2a415a35a40e
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-03 13:18:40 +0300

    add condition internal error in  error middleware

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -8,8 +8,8 @@ module.exports =  function(app){
   app.use((err, req, res, next)=>{
     winston.error(err.message, err);
     const serverError = createErrors.InternalServerError;
-    const status = err?.status || serverError.status;
-    const message = err?.message || serverError.message;
+    const status = err?.status || serverError.status || 500;
+    const message = err?.message || serverError.message || 'InternalServerError';
     res.status(status).json({
       status,
       data: {
