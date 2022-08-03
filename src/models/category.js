c903eb6f2377f6bb4f682d9c85a0ea6333a62f23
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-03 13:14:05 +0300

    add category model

--- src/models/category.js
+++ src/models/category.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
   title : {type: String},
-  parent : {type: [mongoose.Types.ObjectId], ref: 'category', default: undefined},
+  parent : {type: mongoose.Types.ObjectId, default: undefined},
 });
 const Category = mongoose.model('categorie', categorySchema);
 module.exports = {
