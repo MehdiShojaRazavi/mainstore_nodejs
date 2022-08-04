5be97baadfc4fef1368421b36e79da9d1c754705
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-04 21:03:53 +0300

    fix category swagger with requestBody

--- src/routes/admin/category/swagger.js
+++ src/routes/admin/category/swagger.js
@@ -1,6 +1,6 @@
 /**
  * @swagger
- * tags: CategoryPage
+ * tags: admin-panel
  * description: Category page section
  */
 
@@ -8,38 +8,38 @@
  * @swagger
  *  components:
  *    schemas:
- *      Add-Category:
- *        type: object
- *        properties: 
- *          title:
- *            type: string
- *            description: Enter title
- *            example: johndoe123 
- *          parent:
- *            type: string
- *            allowEmptyValue: true
- *            description: Enter parent
+ *       Add-Category:
+ *          type: object
+ *          properties:
+ *            title:
+ *              type: string
+ *              description: Enter unique category
+ *              example: johndoe123 
+ *            parent:
+ *              type: string
+ *              description: Enter category Id
+ *              example: 62ea34a2cac9041c8941636a 
  */
 
 /**
  * @swagger
- *  /admin/category/add:
- *    post:
- *      tags: [CategoryPage]
- *      summary: add category
- *      description: add category
- *      requestBody:
- *        content:
- *          application/x-www-form-urlencoded:
- *            schema:
- *              $ref: '#/components/schemas/Add-Category'
- *      responses:
- *        201:
- *          description: success
- *        400:
- *          description: not found
- *        404:
- *          description: A user with the specified ID was not found.
- *        500:
- *          description: Internal Server Error
+ *   /admin/category/add:
+ *     post:
+ *       tags: [admin-panel]
+ *       summary: add category
+ *       description: add category
+ *       requestBody:
+ *         content:
+ *           application/x-www-form-urlencoded:
+ *             schema:
+ *               $ref: '#/components/schemas/Add-Category'
+ *       responses:
+ *         201:
+ *           description: success
+ *         400:
+ *           description: not found
+ *         404:
+ *           description: A user with the specified ID was not found.
+ *         500:
+ *           description: Internal Server Error
  */
