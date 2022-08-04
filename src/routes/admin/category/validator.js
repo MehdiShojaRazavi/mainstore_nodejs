633abc2fa5f36b26d035c090d63d117e2569daf5
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-04 20:58:48 +0300

    validate parent as objectId with joi.hex()

--- src/routes/admin/category/validator.js
+++ src/routes/admin/category/validator.js
@@ -2,8 +2,8 @@ const joi = require('@hapi/joi');
 const createError = require('http-errors');
 const { MongoIDPattern } = require('../../../utils/constants');
 const addCategorySchema = joi.object({
-  title: joi.string().min(3).max(30).required().error(createError.BadRequest('Title must be between 3 to 30 characters')),
-  parent: joi.string().allow('').pattern(MongoIDPattern).error(createError.BadRequest('parent is invalid'))
+  title: joi.string().min(3).max(30).error(createError.BadRequest('Title must be between 3 to 30 characters')),
+  parent: joi.string().trim().hex().length(24).optional().allow('')
 });
 module.exports ={
   addCategorySchema
