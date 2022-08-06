6895e51182bed67b8a86538079c5c8329f567533
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-06 22:59:41 +0300

    add remove route of category

--- src/routes/admin/category/index.js
+++ src/routes/admin/category/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {Controller} = require('./controller');
 
+router.get('/all', Controller.getAllCategory);
 router.post('/add', Controller.addCategory);
 router.get('/parents', Controller.getAllParents);
 router.get('/children/:parent', Controller.getChildOfParents);
-router.get('/all', Controller.getAllCategory);
-
+router.delete('/remove/:categoryId', Controller.removeCategoryById);
 module.exports = router;
