06b3f51fe65f9a7a9df3a97870c47edf6329af2c
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-07 11:29:58 +0300

    add vlidate to vaidation categoryId in removeCategoryById

--- src/routes/admin/category/validator.js
+++ src/routes/admin/category/validator.js
@@ -5,6 +5,10 @@ const addCategorySchema = joi.object({
   title: joi.string().min(3).max(30).required().error(createError.BadRequest('Title must be between 3 to 30 characters')),
   parent: joi.string().allow('').pattern(MongoIDPattern).error(createError.BadRequest('parent is invalid'))
 });
+const removeCategorySchema = joi.object({
+  categoryId: joi.string().hex().length(24).error(createError.BadRequest('Category id is invalid'))
+});
 module.exports ={
-  addCategorySchema
+  addCategorySchema,
+  removeCategorySchema
 }
