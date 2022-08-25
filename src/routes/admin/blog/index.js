356d696427a3e7332e18a5a71ad8ba7c2e661e3a
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-25 22:45:36 +0300

    add index to blog route

--- src/routes/admin/blog/index.js
+++ src/routes/admin/blog/index.js
@@ -1,7 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const {Controller} = require('./controller');
+const { AdminBlogController } = require("../../http/controllers/admin/blog/blog.controller");
+const { stringToArray } = require("../../http/middlewares/stringToarray");
+const { uploadFile } = require("../../utils/multer");
 
-router.get('/all', Controller.getAllCategory);
+const router = require("express").Router();
+router.get("/", AdminBlogController.getListOfBlogs)
+router.post("/add",uploadFile.single("image"), stringToArray("tags"), AdminBlogController.createBlog)
+router.patch("/update/:id",uploadFile.single("image"), stringToArray("tags"), AdminBlogController.updateBlogById)
+router.get("/:id", AdminBlogController.getOneBlogById);
+router.delete("/:id", AdminBlogController.deleteBlogById);
 
-module.exports = router;
+
+
+module.exports = {
+    AdminApiBlogRouter : router
+}
