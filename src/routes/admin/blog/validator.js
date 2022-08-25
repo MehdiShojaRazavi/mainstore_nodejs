9a702a565c83d12858f4cf6e49f9fba59d2f20bb
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-25 22:47:39 +0300

    add validator to blog route

--- src/routes/admin/blog/validator.js
+++ src/routes/admin/blog/validator.js
@@ -1,14 +1,16 @@
-const joi = require('@hapi/joi');
-const createError = require('http-errors');
-const CreateBlogSchema = joi.object({
-  title: joi.string().min(3).max(30).error(createError.BadRequest('Title must be between 3 to 30 characters')),
-  text: joi.string().error(createError.BadRequest('Text is invalid')),
-  short_text: joi.string().error(createError.BadRequest('Text is invalid')),
-  image: joi.string().error(createError.BadRequest('Image is invalid')),
-  tags: joi.array().min(0).max(20).error(createError.BadRequest('Tags must be at least 20 characters')),
-  category: joi.string().trim().hex().length(24)
+const Joi = require("@hapi/joi");
+const createError = require("http-errors");
+const { MongoIDPattern } = require("../../../utils/constans");
+const createBlogSchema = Joi.object({
+    title : Joi.string().min(3).max(30).error(createError.BadRequest("عنوان دسته بندی صحیح نمیباشد")),
+    text: Joi.string().error(createError.BadRequest("متن ارسال شده صحیح نمیباشد")),
+    short_text: Joi.string().error(createError.BadRequest("متن ارسال شده صحیح نمیباشد")),
+    filename: Joi.string().pattern(/(\.png|\.jpg|\.webp|\.jpeg|\.gif)$/).error(createError.BadRequest("تصویر ارسال شده صحیح نمیباشد")),
+    tags: Joi.array().min(0).max(20).error(createError.BadRequest("برچسب ها نمیتواند بیشتر از 20 ایتم باشد")),
+    category: Joi.string().pattern(MongoIDPattern).error(createError.BadRequest("دسته بندی مورد نظر یافت نشد")),
+    fileUploadPath : Joi.allow()
 });
 
-module.exports ={
-  CreateBlogSchema
+module.exports = {
+    createBlogSchema
 }
