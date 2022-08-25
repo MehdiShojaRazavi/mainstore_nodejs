d036eee7dc44327a55b5a99414e76a3efeb7fcc4
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-25 22:46:58 +0300

    add swagger to blog route

--- src/routes/admin/blog/swagger.js
+++ src/routes/admin/blog/swagger.js
@@ -1,23 +1,136 @@
 /**
  * @swagger
- * tags: admin-panel
- * description: Category page section
+ *  components:
+ *      schemas:
+ *          Blog:
+ *              type: object
+ *              required:
+ *                  -   title
+ *                  -   short_text
+ *                  -   text
+ *                  -   tags
+ *                  -   category
+ *                  -   image
+ *              properties:
+ *                  title:
+ *                      type: string
+ *                      description: the title of category
+ *                  short_text:
+ *                      type: string
+ *                      description: the summary of text of blog
+ *                  text:
+ *                      type: string
+ *                      description: the text of blog
+ *                  tags:
+ *                      type: string
+ *                      description: the list of tags for example(tag1#tag2#tag_foo)
+ *                  category:
+ *                      type: string
+ *                      description: the id of category for foreinField in blog
+ *                  image:
+ *                      type: file
+ *                      description: the index picture of blog
+ *          BlogUpdate:
+ *              type: object
+ *              properties:
+ *                  title:
+ *                      type: string
+ *                      description: the title of category
+ *                  short_text:
+ *                      type: string
+ *                      description: the summary of text of blog
+ *                  text:
+ *                      type: string
+ *                      description: the text of blog
+ *                  tags:
+ *                      type: string
+ *                      description: the list of tags for example(tag1#tag2#tag_foo)
+ *                  category:
+ *                      type: string
+ *                      description: the id of category for foreinField in blog
+ *                  image:
+ *                      type: file
+ *                      description: the index picture of blog
+ */ 
+
+
+/**
+ * @swagger
+ *  /admin/blogs:
+ *      get:
+ *          tags: [ Blog(AdminPanel)]
+ *          summary: get all blogs
+ *          responses:
+ *              200:
+ *                  description: success - get array of blogs
+ */
+/**
+ * @swagger
+ *  /admin/blogs/add:
+ *      post:
+ *          tags: [ Blog(AdminPanel)]
+ *          summary: create Blog document
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  multipart/form-data:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Blog'
+ *          responses:
+ *              201:
+ *                  description: created
+ */
+/**
+ * @swagger
+ *  /admin/blogs/update/{id}:
+ *      patch:
+ *          tags: [ Blog(AdminPanel)]
+ *          summary: update  Blog document by id 
+ *          consumes: 
+ *              -   multipart/form-data
+ *          parameters:
+ *              -   in: path
+ *                  required: true
+ *                  name: id
+ *                  type: string
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  multipart/form-data:
+ *                      schema:
+ *                          $ref: '#/components/schemas/BlogUpdate'
+ *          responses:
+ *              200:
+ *                  description: success
  */
 
 /**
  * @swagger
- *  /admin/blogs/all:
- *    get:
- *      tags: [CategoryPage]
- *      summary: get all item of blogs
- *      description: get all category
- *      responses:
- *        201:
- *          description: success
- *        400:
- *          description: not found
- *        404:
- *          description: A children of category with the specified ParentID was not found.
- *        500:
- *          description: Internal Server Error
+ *  /admin/blogs/{id}:
+ *      get:
+ *          summary: get blog by ID and populate this field 
+ *          tags: [ Blog(AdminPanel) ]
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  type: string
+ *                  required: true
+ *          responses:
+ *              200:
+ *                  description: success
+ */
+/**
+ * @swagger
+ *  /admin/blogs/{id}:
+ *      delete:
+ *          summary: remove blog by ID 
+ *          tags: [ Blog(AdminPanel) ]
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  type: string
+ *                  required: true
+ *          responses:
+ *              200:
+ *                  description: success
  */
